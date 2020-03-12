13d18affb4f5dfa825f944b7a75bf495ff0c5e63
Gianluca Marzaro <3406023+hardweb-it@users.noreply.github.com>
2020-03-12 14:18:40 +0100

    sync master with stable branch

---
+++ hw-manutenzione.php
@@ -0,0 +1,88 @@
+<?php
+/*
+* Plugin Name: Manutenzione by Hardweb.it
+* Plugin URI:  https://www.hardweb.it
+* Description: Plugin di gestione manutenzione portali
+* Version:     2.1.0
+* Author:      Hardweb.it
+* Author URI:  https://www.hardweb.it
+* Copyright: © 2020 Hardweb IT
+* License:      GPL2
+* License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+*/
+#SECURITY CHECK
+if(!defined('ABSPATH')) exit;
+#DEFINE
+define( 'HW_MANUTENZIONE_PLUGIN_VERSION', '2.1.0' ); //VERSION
+define( 'HW_MANUTENZIONE_REPO_URL', 'http://hardweb.it' );
+define( 'HW_MANUTENZIONE_ITEM_ID', 2151 );
+define( 'HW_MANUTENZIONE_PLUGIN_LICENSE_PAGE', 'hw-manutenzione-license' );
+define( 'HW_MANUTENZIONE_ITEM_NAME', 'Manutenzione by Hardweb.it' );
+define( 'HW_MANUTENZIONE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'HW_MANUTENZIONE_PLUGIN_DIR', dirname( __FILE__ ) );
+define( 'HW_MANUTENZIONE_PLUGIN_FULLPATH', dirname( __FILE__ ).'/hw-manutenzione.php');
+#INCLUDE
+include_once(HW_MANUTENZIONE_PLUGIN_DIR.'/includes/hw-manutenzione-functions.php');
+#UPDATER
+include_once(HW_MANUTENZIONE_PLUGIN_DIR.'/includes/hw-manutenzione-plugin-updater.php');
+
+function hw_manutenzione_license_menu() {
+	add_management_page( 'Manutenzione by Hardweb.it', 'Manutenzione by Hardweb.it', 'manage_options', HW_MANUTENZIONE_PLUGIN_LICENSE_PAGE, 'hw_manutenzione_license_page' );
+}
+add_action('admin_menu', 'hw_manutenzione_license_menu');
+function hw_manutenzione_license_page() {
+	$license = get_option( 'hw_manutenzione_license_key' );
+	$status  = get_option( 'hw_manutenzione_license_status' );
+	if( $status !== false && $status == 'valid' ) {
+		$license_message = "<span style='color:green;'>Attiva</span>";
+	} else {
+		$license_message = "<span style='color:black;'>Inserisci la tua chiave di licenza</span>";
+	}
+	?>
+	<div class="wrap">
+		<h2><?php _e('Licenza Manutenzione'); ?></h2>
+		<form method="post" action="options.php">
+			<?php settings_fields('hw_manutenzione_license'); ?>
+			<table class="form-table">
+				<tbody>
+					<tr valign="top">
+						<th scope="row" valign="top">
+							<?php _e('Chiave di Licenza'); ?>
+						</th>
+						<td>
+							<input id="hw_manutenzione_license_key" name="hw_manutenzione_license_key" type="password" class="regular-text" value="<?php esc_attr_e( $license ); ?>" />
+							<label class="description" for="hw_manutenzione_license_key"><?php echo $license_message; ?></label>
+						</td>
+					</tr>
+					<tr valign="top">
+						<th scope="row" valign="top">
+							<?php _e('Scadenza'); ?>
+						</th>
+						<td>
+							<label class="description" for="hw_manutenzione_license_expire"><?php echo hw_manutenzione_get_license_message(); ?></label>
+						</td>
+					</tr>					
+					<?php if( false !== $license ) { ?>
+						<tr valign="top">
+							<th scope="row" valign="top">
+								<?php _e('Azioni'); ?>
+							</th>
+							<td>
+								<?php wp_nonce_field( 'hw_manutenzione_nonce', 'hw_manutenzione_nonce' ); ?>
+								<?php if( $status !== false && $status == 'valid' ) { ?>
+									<input type="submit" class="button-secondary" name="edd_license_deactivate" value="<?php _e('Disattiva licenza'); ?>"/>
+								<?php } else { ?>
+									<input type="submit" class="button-secondary" name="edd_license_activate" value="<?php _e('Attiva licenza'); ?>"/>
+								<?php } ?>
+							</td>
+						</tr>
+					<?php } ?>
+				</tbody>
+			</table>
+			<?php submit_button('Aggiorna la chiave di Licenza'); ?>
+		</form>
+	<?php hw__manutenzione_page(); ?>
+	</div>
+	<?php
+}
+?>
