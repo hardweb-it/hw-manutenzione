960d972cd01f3ae192e54df5980df190c211b693
Gianluca Marzaro <3406023+hardweb-it@users.noreply.github.com>
2020-03-13 03:54:52 +0100

    add folders

---
+++ includes/hw-manutenzione-functions.php
@@ -0,0 +1,133 @@
+<?php
+#Check license status
+$status  = get_option( 'hw_manutenzione_license_status' );
+$licstat = ($status == 'valid') ? true : false;
+define('HW_MANUTENZIONE_LICENSE_STATUS', $licstat);
+# ADMIN FOOTER CONTACTS
+add_action('in_admin_footer', 'hw_admin_custom_footer');
+function hw_admin_custom_footer() {
+$expire_check_message = ' | ' . hw_manutenzione_get_license_message();
+echo '
+<div id="footer">
+	<div id="footer-bottom">
+		<p>Questo sito &egrave; gestito da Hardweb.it © 2017 - '. date('Y', time()).' | <a href="mailto:assistenza@hardweb.it">assistenza@hardweb.it</a> | <a href="https://hardweb.it" target="_blank">hardweb.it</a> | <a href="tel:3891671634" target="_blank">+39 389 1671634</a> | <a style="color:#1EBEA5" href="https://wa.me/393287759424" target="_blank">Whatsapp business</a>'.$expire_check_message.'</p>
+	</div>
+</div>';
+}
+
+#CALLED FROM MAIN PLUGIN
+function hw__manutenzione_page() {
+#HIDE ITEMS TO non-hardweb users
+if (hw_is_hardweb_user()) {	
+    // Get theme info
+    $theme_data = wp_get_theme();
+    $theme = $theme_data->Name . ' ' . $theme_data->Version;
+
+    // Get plugins that have an update
+    $updates = get_plugin_updates();
+	
+    // WordPress active plugins
+    $plugins = get_plugins();
+    $active_plugins = get_option('active_plugins', array());
+	echo "<div style='margin-top: 50px;'><h3>Plugin da aggiornare</h3></div>";
+	echo "<div style='-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;width: 60%;min-height:200px'>Eseguiti aggiornamenti plugin:<br>
+	<ul>";
+    foreach ($plugins as $plugin_path => $plugin) {
+        if (!in_array($plugin_path, $active_plugins)) {
+            continue;
+        }
+        if (array_key_exists($plugin_path, $updates)) {
+			echo '<li>'.$plugin['Name'] . ' v' . $plugin['Version'] . ' -> v' . $updates[$plugin_path]->update->new_version .'</li>';
+		}
+    }
+	echo "</ul></div>";
+}
+}
+
+#FUNZIONE PER LA RIMOZIONE DI VOCI DAL MENU ADMIN LATERALE
+add_action( 'admin_menu', 'hw__manutenzione_remove_menu_items' );
+function hw__manutenzione_remove_menu_items() {
+	
+	#HIDE ITEMS TO non-hardweb users
+	if (hw_is_hardweb_user()) {
+
+		//menu pages
+		$hide_these_pages = array('the7-dashboard','settings_page','vc-general','pb_backupbuddy_backup','Wordfence','about-ultimate');
+		foreach($hide_these_pages as $id=>$slug) {
+			remove_menu_page($slug);
+		}
+
+		//submenu pages
+		$hide_these_submenu_pages = array('tools.php'=>'aiosp_import');
+		foreach($hide_these_submenu_pages as $main_page=>$sub_slug) {
+			remove_submenu_page($main_page, $sub_slug);
+		}
+		
+	}
+}
+
+add_action('pre_current_active_plugins', 'hw_manutenzione_hide_plugins');
+function hw_manutenzione_hide_plugins() {
+	# hide plugins from main list
+	global $wp_list_table;
+	$hidearr = array('hw-manutenzione/index.php');
+	$myplugins = $wp_list_table->items;
+	foreach ($myplugins as $key => $val) {
+		if (in_array($key,$hidearr)) {
+		  unset($wp_list_table->items[$key]);
+		}
+	}	
+}
+//Load some admin styles
+add_action('admin_enqueue_scripts', 'hw_manutenzione_admin_style');
+function hw_manutenzione_admin_style() {
+  wp_enqueue_style('hw-maintenance-style', HW_MANUTENZIONE_PLUGIN_URL .'/css/hwmaint.css', null, null, 'all');
+}
+
+# CHECK IF CURRENT USER IS 'hardweb'
+function hw_is_hardweb_user() {
+	$current_user = wp_get_current_user();
+	if ($current_user->user_login == "hardweb") {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/**
+ * Debug Pending Updates
+ *
+ * Crude debugging method that will spit out all pending plugin
+ * and theme updates for admin level users when ?debug_updates is
+ * added to a /wp-admin/ URL.
+ */
+function hw_debug_pending_updates() {
+
+    // Rough safety nets
+    if ( ! is_user_logged_in() || ! current_user_can( 'manage_options' ) ) return;
+    if ( ! isset( $_GET['debug_updates'] ) ) return;
+
+    $output = "";
+
+    // Check plugins
+    $plugin_updates = get_site_transient( 'update_plugins' );
+    if ( $plugin_updates && ! empty( $plugin_updates->response ) ) {
+        foreach ( $plugin_updates->response as $plugin => $details ) {
+            $output .= "<p><strong>Plugin</strong> <u>$plugin</u> is reporting an available update.</p>";
+        }
+    }
+
+    // Check themes
+    wp_update_themes();
+    $theme_updates = get_site_transient( 'update_themes' );
+    if ( $theme_updates && ! empty( $theme_updates->response ) ) {
+        foreach ( $theme_updates->response as $theme => $details ) {
+            $output .= "<p><strong>Theme</strong> <u>$theme</u> is reporting an available update.</p>";
+        }
+    }
+
+    if ( empty( $output ) ) $output = "No pending updates found in the database.";
+	echo $output;
+    wp_die( $output );
+}
+?>
---
+++ includes/hw-manutenzione-plugin-updater.php
@@ -0,0 +1,296 @@
+<?php
+#UPDATE CLASS
+if( !class_exists( 'HW_Plugin_Class_Updater' ) ) {
+	// load our custom updater
+	include( HW_MANUTENZIONE_PLUGIN_DIR . '/includes/HW_Plugin_Class_Updater.php' );
+}
+
+add_action( 'admin_init', 'hw_manutenzione_updater', 0 );
+function hw_manutenzione_updater() {
+
+	// retrieve our license key from the DB
+	$license_key = trim( get_option( 'hw_manutenzione_license_key' ) );
+
+	// setup the updater
+	$edd_updater = new HW_Plugin_Class_Updater( HW_MANUTENZIONE_REPO_URL, HW_MANUTENZIONE_PLUGIN_FULLPATH,
+		array(
+			'version' => HW_MANUTENZIONE_PLUGIN_VERSION,	// current version number
+			'license' => $license_key,             			// license key (used get_option above to retrieve from DB)
+			'item_id' => HW_MANUTENZIONE_ITEM_ID,       	// ID of the product
+			'author'  => 'Hardweb.it', 						// author of this plugin
+			'beta'    => false,
+		)
+	);
+	
+	/*
+	if (WP_DEBUG_DISPLAY) {
+		echo "<div style='margin-left:200px;'><h3>HW Manutenzione debug info:</h3><br>";
+		print_r($edd_updater);
+		echo "</div>";
+	}
+	*/
+
+}
+function hw_manutenzione_register_option() {
+	// creates our settings in the options table
+	register_setting('hw_manutenzione_license', 'hw_manutenzione_license_key', 'edd_sanitize_license' );
+}
+add_action('admin_init', 'hw_manutenzione_register_option');
+function edd_sanitize_license( $new ) {
+	$old = get_option( 'hw_manutenzione_license_key' );
+	if( $old && $old != $new ) {
+		delete_option( 'hw_manutenzione_license_status' ); // new license has been entered, so must reactivate
+	}
+	return $new;
+}
+
+function hw_manutenzione_get_license_message() {
+	// retrieve the license from the database
+	$license = trim( get_option( 'hw_manutenzione_license_key' ) );
+	// data to send in our API request
+	$api_params = array(
+		'edd_action' => 'activate_license',
+		'license'    => $license,
+		'item_id' => HW_MANUTENZIONE_ITEM_ID,
+		'item_name'  => urlencode( HW_MANUTENZIONE_ITEM_NAME ), // the name of our product in EDD
+		'url'        => home_url()
+	);
+	// return message
+	$message = "";
+	// Call the custom API.
+	$response = wp_remote_post( HW_MANUTENZIONE_REPO_URL, array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+		// make sure the response came back okay
+		if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+			//error
+		} else {
+			$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+			if ( false === $license_data->success ) {
+				switch( $license_data->error ) {
+					case 'expired' :
+						$message = sprintf(
+							__( '<span style="color:#ff0000">SERVIZIO SCADUTO IL %s</span>' ),
+							date_i18n( get_option( 'date_format' ), strtotime( $license_data->expires, current_time( 'timestamp' ) ) )
+						);
+						break;
+					case 'no_activations_left':
+						$message = __( 'Questa licenza ha raggiunto il limite massimo di attivazioni.' );
+						break;						
+					default :
+						$message = __( 'SERVIZIO SCADUTO' );
+						break;
+				}
+			} elseif ($license_data->expires == 'lifetime') {
+				
+				$message = __( 'SERVIZIO ATTIVO SENZA SCADENZA' );				
+				
+			} else {
+				
+				$message = sprintf(
+					__( 'SERVIZIO ATTIVO FINO AL %s' ),
+					date_i18n( get_option( 'date_format' ), strtotime( $license_data->expires, current_time( 'timestamp' ) ) )
+				);
+			}
+		}
+		//print_r($license_data);
+	return $message;
+}
+
+/************************************
+* this illustrates how to activate
+* a license key
+*************************************/
+function hw_manutenzione_activate_license() {
+	// listen for our activate button to be clicked
+	if( isset( $_POST['edd_license_activate'] ) ) {
+		// run a quick security check
+	 	if( ! check_admin_referer( 'hw_manutenzione_nonce', 'hw_manutenzione_nonce' ) )
+			return; // get out if we didn't click the Activate button
+		// retrieve the license from the database
+		$license = trim( get_option( 'hw_manutenzione_license_key' ) );
+		// data to send in our API request
+		$api_params = array(
+			'edd_action' => 'activate_license',
+			'license'    => $license,
+			'item_id' => HW_MANUTENZIONE_ITEM_ID,
+			'item_name'  => urlencode( HW_MANUTENZIONE_ITEM_NAME ), // the name of our product in EDD
+			'url'        => home_url()
+		);
+		// Call the custom API.
+		$response = wp_remote_post( HW_MANUTENZIONE_REPO_URL, array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+		// make sure the response came back okay
+		if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+			if ( is_wp_error( $response ) ) {
+				$message = $response->get_error_message();
+			} else {
+				$message = __( 'Si è verificato un errore. Riprova.' );
+			}
+		} else {
+			$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+			if ( false === $license_data->success ) {
+				switch( $license_data->error ) {
+					case 'expired' :
+						$message = sprintf(
+							__( 'La tua licenza è scaduta il %s.' ),
+							date_i18n( get_option( 'date_format' ), strtotime( $license_data->expires, current_time( 'timestamp' ) ) )
+						);
+						break;
+					case 'disabled' :
+					case 'revoked' :
+						$message = __( 'La tua licenza è stata disabilitata.' );
+						break;
+					case 'missing' :
+						$message = __( 'Licenza non valida.' );
+						break;
+					case 'invalid' :
+					case 'site_inactive' :
+						$message = __( 'La tua licenza non è abilitata per questo sito.' );
+						break;
+					case 'item_name_mismatch' :
+						$message = sprintf( __( 'La licenza inserita non è valida per %s.' ), HW_MANUTENZIONE_ITEM_NAME );
+						break;
+					case 'no_activations_left':
+						$message = __( 'Questa licenza ha raggiunto il limite massimo di attivazioni.' );
+						break;
+					default :
+						$message = __( 'Si è verificato un errore. Riprova.' );
+						break;
+				}
+			}
+		}
+		// Check if anything passed on a message constituting a failure
+		if ( ! empty( $message ) ) {
+			$base_url = admin_url( 'plugins.php?page=' . HW_MANUTENZIONE_PLUGIN_LICENSE_PAGE );
+			$redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
+			wp_redirect( $redirect );
+			exit();
+		}
+		// $license_data->license will be either "valid" or "invalid"
+		update_option( 'hw_manutenzione_license_status', $license_data->license );
+		wp_redirect( admin_url( 'plugins.php?page=' . HW_MANUTENZIONE_PLUGIN_LICENSE_PAGE ) );
+		exit();
+	}
+}
+add_action('admin_init', 'hw_manutenzione_activate_license');
+/***********************************************
+* Illustrates how to deactivate a license key.
+* This will decrease the site count
+***********************************************/
+function hw_manutenzione_deactivate_license() {
+	// listen for our activate button to be clicked
+	if( isset( $_POST['edd_license_deactivate'] ) ) {
+		// run a quick security check
+	 	if( ! check_admin_referer( 'hw_manutenzione_nonce', 'hw_manutenzione_nonce' ) )
+			return; // get out if we didn't click the Activate button
+		// retrieve the license from the database
+		$license = trim( get_option( 'hw_manutenzione_license_key' ) );
+		// data to send in our API request
+		$api_params = array(
+			'edd_action' => 'deactivate_license',
+			'license'    => $license,
+			'item_id' => HW_MANUTENZIONE_ITEM_ID,
+			'item_name'  => urlencode( HW_MANUTENZIONE_ITEM_NAME ), // the name of our product in EDD
+			'url'        => home_url()
+		);
+		// Call the custom API.
+		$response = wp_remote_post( HW_MANUTENZIONE_REPO_URL, array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+		// make sure the response came back okay
+		if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+			if ( is_wp_error( $response ) ) {
+				$message = $response->get_error_message();
+			} else {
+				$message = __( 'Si è verificato un errore. Riprova.' );
+			}
+			$base_url = admin_url( 'plugins.php?page=' . HW_MANUTENZIONE_PLUGIN_LICENSE_PAGE );
+			$redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
+			wp_redirect( $redirect );
+			exit();
+		}
+		// decode the license data
+		$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+		// $license_data->license will be either "deactivated" or "failed"
+		if( $license_data->license == 'deactivated' ) {
+			delete_option( 'hw_manutenzione_license_status' );
+		}
+		wp_redirect( admin_url( 'plugins.php?page=' . HW_MANUTENZIONE_PLUGIN_LICENSE_PAGE ) );
+		exit();
+	}
+}
+add_action('admin_init', 'hw_manutenzione_deactivate_license');
+/************************************
+* this illustrates how to check if
+* a license key is still valid
+* the updater does this for you,
+* so this is only needed if you
+* want to do something custom
+*************************************/
+function hw_manutenzione_check_license() {
+	global $wp_version;
+	$license = trim( get_option( 'hw_manutenzione_license_key' ) );
+	$api_params = array(
+		'edd_action' => 'check_license',
+		'license' => $license,
+		'item_id' => HW_MANUTENZIONE_ITEM_ID,
+		'item_name' => urlencode( HW_MANUTENZIONE_ITEM_NAME ),
+		'url'       => home_url()
+	);
+	// Call the custom API.
+	$response = wp_remote_post( HW_MANUTENZIONE_REPO_URL, array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+	if ( is_wp_error( $response ) )
+		return false;
+	$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+	if( $license_data->license == 'valid' ) {
+		echo 'Valida'; exit;
+		// this license is still valid
+	} else {
+		echo 'Non valida'; exit;
+		// this license is no longer valid
+	}
+}
+
+function hw_manutenzione_get_check_license() {
+	global $wp_version;
+	$license = trim( get_option( 'hw_manutenzione_license_key' ) );
+	$api_params = array(
+		'edd_action' => 'check_license',
+		'license' => $license,
+		'item_id' => HW_MANUTENZIONE_ITEM_ID,
+		'item_name' => urlencode( HW_MANUTENZIONE_ITEM_NAME ),
+		'url'       => home_url()
+	);
+	// Call the custom API.
+	$response = wp_remote_post( HW_MANUTENZIONE_REPO_URL, array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+	if ( is_wp_error( $response ) )
+		return false;
+	$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+	if( $license_data->license == 'valid' ) {
+		return true;
+		// this license is still valid
+	} else {
+		return false;
+		// this license is no longer valid
+	}
+}
+
+/**
+ * This is a means of catching errors from the activation method above and displaying it to the customer
+ */
+function hw_manutenzione_admin_notices() {
+	if ( isset( $_GET['sl_activation'] ) && ! empty( $_GET['message'] ) ) {
+		switch( $_GET['sl_activation'] ) {
+			case 'false':
+				$message = urldecode( $_GET['message'] );
+				?>
+				<div class="error">
+					<p><?php echo $message; ?></p>
+				</div>
+				<?php
+				break;
+			case 'true':
+			default:
+				// Developers can put a custom success message here for when activation is successful if they way.
+				break;
+		}
+	}
+}
+add_action( 'admin_notices', 'hw_manutenzione_admin_notices' );
+?>
